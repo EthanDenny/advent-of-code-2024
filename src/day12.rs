5cc4b7a6dcacbd37a98bbc91201d1cf3f0b21d8d
Ethan Denny <ethandenny@protonmail.com>
2024-12-12 16:23:59 -0330

    Utils

---
+++ src/day12.rs
@@ -0,0 +1,17 @@
+pub fn answers(input: Vec<String>) -> (i32, i32) {
+    // Parsing
+
+    for line in input {}
+
+    // Answer 1
+
+    let mut ans1 = 0;
+
+    // Answer 2
+
+    let mut ans2 = 0;
+
+    // Return
+
+    (ans1, ans2)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,55 @@
+mod day12;
+
+use chrono::{Datelike, FixedOffset, Utc};
+use std::env;
+use std::fs;
+
+// Adapted from the documentation
+fn read_lines(day: i32) -> Vec<String> {
+    let path = format!("input/day{day}.txt");
+    let message = format!("Couldn't open input for Day {day}");
+
+    fs::read_to_string(&path)
+        .expect(&message)
+        .lines()
+        .map(String::from)
+        .collect()
+}
+
+macro_rules! convert_ans_to_string {
+    ($ans:expr) => {{
+        let ans = $ans;
+        (ans.0.to_string(), ans.1.to_string())
+    }};
+}
+
+fn get_answers(input: Vec<String>, day: i32) -> (String, String) {
+    match day {
+        12 => convert_ans_to_string!(day12::answers(input)),
+        _ => unimplemented!(),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let eastern = FixedOffset::west_opt(5 * 3600).unwrap();
+    let date = Utc::now().with_timezone(&eastern).day() as i32;
+
+    let day: i32 = {
+        if let Some(arg) = env::args().nth(1) {
+            let arg = arg.parse().unwrap();
+            if arg > date {
+                panic!("It isn't Day {arg} yet!");
+            } else {
+                arg
+            }
+        } else {
+            date
+        }
+    };
+
+    let input = read_lines(day);
+    let (ans1, ans2) = get_answers(input, day);
+
+    println!("Answer 1: {ans1}");
+    println!("Answer 2: {ans2}");
 }
